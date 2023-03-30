5ab10e47de584ead57cacd58b936ca6c9509e203
taling21 <taling21@iitk.ac.in>
2023-03-30 12:41:19 +0530

    removing updated events

--- backend/routes/homepg.js
+++ backend/routes/homepg.js
@@ -5,7 +5,28 @@ const Event = require('../models/Event');
 // ROUTE 1: Get All the Notes using: GET "/api/events/getuser". Login required
 router.get('/allevents', async (req, res) => { // not user specific 
     try {
-        const events = await Event.find().sort({ date: 1 });  // sort in ascending order of date
+        const now = new Date();
+        // console.log(now.toLocaleTimeString())
+        console.log(now)
+        //const currentTime = now.toLocaleTimeString();
+        let event = {};
+        
+        const outdated = await Event.find({ endTime:{$lt : now}});
+        for(let i=0;i<outdated.length;i++){
+            console.log(outdated[i]);
+            event = await Event.findByIdAndDelete(outdated[i]._id);
+        }
+        var events = await Event.find().sort({ endTime: 1 });  // sort in ascending order of date
+        // for(let i=0;i<events.length;i++){
+        //     event = Event.findByIdAndDelete(outdated[i]._id);
+        // // }
+        // console.log(events.length)
+        // console.log(events[0].endTime)
+        // console.log(events[0].endTime < now.toLocaleTimeString())
+        // while(events.length > 0 && events[0].endTime < now){
+        //     console.log(events.length )
+        //     events = Event.findByIdAndDelete(events[0]._id);
+        // }
         res.json({len:events.length,events:events})
     } catch (error) {
         console.error(error.message);
